7711e05210cd424601d4600f1552af528cd27829
MDN <mark.denardo@gmail.com>
2023-09-15 12:16:58 -0400

    Create findyourstrength.js

---
+++ findyourstrength.js
@@ -0,0 +1,14 @@
+// FIND YOUR STRENGTH IN LOVE
+// #whitneyforever
+// MDN 2023
+const f=(()=>a.fft[0])
+s0.initVideo("https://media2.giphy.com/media/nXebtbDWhJoBxvBOse/giphy.mp4")
+s1.initVideo("https://media4.giphy.com/media/FnbXsuq3T7M9NGVn6f/giphy.mp4")
+s2.initVideo("https://media3.giphy.com/media/XWxa19BWu4XwWORdwG/giphy.mp4")
+s3.initVideo("https://media3.giphy.com/media/7MxK1647EtHTfLrrXr/giphy.mp4")
+src(s1).blend(s1,f).add(s2,0.1).add(src(s3).pixelate(100,100),1).out(o0)
+render(o0)
+src(s3).blend(src(s3).pixelate(50,50),f).out(o3)
+render(o3)
+src(o0).blend(o3).out(o2)
+render(o2)
