d4e05925c29377ede830ce5b082479519a3264c2
MDN <mark.denardo@gmail.com>
2023-09-15 13:03:35 -0400

    Create strength-00.js

---
+++ strength-00.js
@@ -0,0 +1,7 @@
+// FIND YOUR STRENGTH IN LOVE
+// #whitneyforever
+// MDN 2023
+const f=(()=>a.fft[0])
+s0.initVideo("https://media2.giphy.com/media/nXebtbDWhJoBxvBOse/giphy.mp4")
+s1.initVideo("https://media4.giphy.com/media/FnbXsuq3T7M9NGVn6f/giphy.mp4")
+src(s1).blend(src(s0).pixelate(100,100),f).out(o0)
